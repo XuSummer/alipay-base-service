0d32750ac96bee5b700e48198da8062150c5fcae
Summer <wlwshlezh@126.com>
2017-05-18 19:39:43 +0800

    无用数据删除

--- src/main/java/com/tools/alipay/App.java
+++ src/main/java/com/tools/alipay/App.java
@@ -2,12 +2,10 @@ package com.tools.alipay;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("en");
     }
 }
